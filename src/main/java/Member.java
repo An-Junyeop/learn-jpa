c3062abac597167770e0baa41b1a0be9b46d9a99
An-Junyeop <thwo0075@gmail.com>
2020-07-02 14:47:43 +0900

    기본 설정 구성
    - Member.java 생성
    - Main.java 생성
    - persistence.xml 생성 및 기본 설정 구성
    - pom.xml dependencies 추가

---
+++ src/main/java/Member.java
@@ -0,0 +1,18 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private Integer age;
+}
