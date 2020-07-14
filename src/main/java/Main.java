6bbd6187e0028fb40da43ec2589ed99b33aa1ba2
An-Junyeop <thwo0075@gmail.com>
2020-07-14 15:09:45 +0900

    Criteria 간단 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class Main {
@@ -14,19 +17,19 @@ public class Main {
 
         tx.begin();
 
-        for(int i = 1; i <= 10; i ++) {
-            Member member = new Member();
-            member.setName("member_" + i);
-            em.persist(member);
-        }
+        // Criteria 사용 준비
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-        String jpql = "select m from Member as m where m.name = 'member_2'";
-        List<Member> resultList = em
-                .createQuery(jpql, Member.class)
-                .getResultList();
+        // 루트 클래스
+        Root<Member> m = query.from(Member.class);
 
-        for (Member member : resultList) {
-            System.out.println("member.id = " + member.getId() + ", member.name = " + member.getName());
+        // 쿼리 생성
+        CriteriaQuery<Member>  cq = query.select(m).where(cb.equal(m.get("name"), "member_3"));
+        List<Member> resultList = em.createQuery(cq).getResultList();
+
+        for(Member member : resultList) {
+            System.out.println(member.toString());
         }
 
         tx.commit();
