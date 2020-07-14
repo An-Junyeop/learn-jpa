3db17de616313b99b392e10fab225d2b1d4c985d
An-Junyeop <thwo0075@gmail.com>
2020-07-14 17:03:51 +0900

    getSingleResult 예제
    - 단일 엔티티 조회
    - 결과가 없는 경우
    - 결과가 한개 초과인 겨웅

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,29 +9,38 @@ public class Main {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
 
-        tx.begin();
+        try {
+            tx.begin();
 
-        // TypedQuery
-        TypedQuery typedQuery = em.createQuery("select m from Member m", Member.class);
-        List<Member> resultList = typedQuery.getResultList();
-
-        for(Member member : resultList) {
+            // getSingleResult
+            Member member = em.createQuery("select m from Member m where m.id = :id", Member.class)
+                    .setParameter("id", 13L)
+                    .getSingleResult();
             System.out.println(member.toString());
+
+            // getSingleResult - 검색 결과가 없는 경우
+//            Member member2 = em.createQuery("select m from Member m where m.id = :id", Member.class)
+//                    .setParameter("id", 1L)
+//                    .getSingleResult();
+//            System.out.println(member.toString());
+
+            // getSingleResult - 검색 결과가 한개 초과인 경우
+            Member member3 = em.createQuery("select m from Member m", Member.class)
+                    .getSingleResult();
+            System.out.println(member.toString());
+
+
+
+            tx.commit();
+        } catch (NoResultException e) {
+            System.out.println("검색 결과가 없을 경우");
+            tx.rollback();
+        } catch (NonUniqueResultException e) {
+            System.out.println("검색 결과가 한개 초과인 경우");
+        } finally {
+            em.close();
         }
 
-        // Query
-        Query query = em.createQuery("select m.id, m.name from Member m");
-        List resultList2 = query.getResultList();
-
-        for (Object o : resultList2) {
-            Object[] result = (Object[]) o; // 결과가 둘 이상이면 Object[]를 반환
-            System.out.println("member.id = " + result[0]);     // JPQL에 쓰여진 순서대로 매핑
-            System.out.println("member.name = " + result[1]);   // JPQL에 쓰여진 순서대로 매핑
-        }
-
-        tx.commit();
-
-        em.close();
         emf.close();
     }
 }
