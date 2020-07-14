fbc4a47c7a00a1a264278804c7d11ec1778adaad
An-Junyeop <thwo0075@gmail.com>
2020-07-14 17:10:24 +0900

    model package clear

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-import model.Member;
-
 import javax.persistence.*;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,31 +9,12 @@ public class Main {
         try {
             tx.begin();
 
-            // getSingleResult
-            Member member = em.createQuery("select m from Member m where m.id = :id", Member.class)
-                    .setParameter("id", 13L)
-                    .getSingleResult();
-            System.out.println(member.toString());
-
-            // getSingleResult - 검색 결과가 없는 경우
-//            Member member2 = em.createQuery("select m from Member m where m.id = :id", Member.class)
-//                    .setParameter("id", 1L)
-//                    .getSingleResult();
-//            System.out.println(member.toString());
-
-            // getSingleResult - 검색 결과가 한개 초과인 경우
-            Member member3 = em.createQuery("select m from Member m", Member.class)
-                    .getSingleResult();
-            System.out.println(member.toString());
-
 
 
             tx.commit();
-        } catch (NoResultException e) {
-            System.out.println("검색 결과가 없을 경우");
+        } catch (Exception e) {
+            e.printStackTrace();
             tx.rollback();
-        } catch (NonUniqueResultException e) {
-            System.out.println("검색 결과가 한개 초과인 경우");
         } finally {
             em.close();
         }
