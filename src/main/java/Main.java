c836415735a539901050a2f996038ea58057fb23
An-Junyeop <thwo0075@gmail.com>
2020-07-14 15:17:43 +0900

    네이티브 SQL 사용 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class Main {
@@ -17,16 +14,11 @@ public class Main {
 
         tx.begin();
 
-        // Criteria 사용 준비
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Member> query = cb.createQuery(Member.class);
+        // 네이티브 SQL
+        String sql = "select id, name from MEMBER where name = 'member_2'";
+        List<Member> resultList = em.createNativeQuery(sql, Member.class).getResultList();
 
-        // 루트 클래스
-        Root<Member> m = query.from(Member.class);
 
-        // 쿼리 생성
-        CriteriaQuery<Member>  cq = query.select(m).where(cb.equal(m.get("name"), "member_3"));
-        List<Member> resultList = em.createQuery(cq).getResultList();
 
         for(Member member : resultList) {
             System.out.println(member.toString());
