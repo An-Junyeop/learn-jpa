8e018dbabde7d74912c2138181c0e313b05fa6ec
An-Junyeop <thwo0075@gmail.com>
2020-07-14 16:57:45 +0900

    TypedQuery와 Query의 차이점

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import model.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class Main {
@@ -14,14 +11,22 @@ public class Main {
 
         tx.begin();
 
-        // 네이티브 SQL
-        String sql = "select id, name from MEMBER where name = 'member_2'";
-        List<Member> resultList = em.createNativeQuery(sql, Member.class).getResultList();
-
-
+        // TypedQuery
+        TypedQuery typedQuery = em.createQuery("select m from Member m", Member.class);
+        List<Member> resultList = typedQuery.getResultList();
 
         for(Member member : resultList) {
             System.out.println(member.toString());
+        }
+
+        // Query
+        Query query = em.createQuery("select m.id, m.name from Member m");
+        List resultList2 = query.getResultList();
+
+        for (Object o : resultList2) {
+            Object[] result = (Object[]) o; // 결과가 둘 이상이면 Object[]를 반환
+            System.out.println("member.id = " + result[0]);     // JPQL에 쓰여진 순서대로 매핑
+            System.out.println("member.name = " + result[1]);   // JPQL에 쓰여진 순서대로 매핑
         }
 
         tx.commit();
