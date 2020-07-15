35bb7452eb984f3000ec34ae62075d27aa8bb0a2
An-Junyeop <thwo0075@gmail.com>
2020-07-15 10:52:15 +0900

    OUTER JOIN 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,14 +16,12 @@ public class Main {
 
             // JOIN
             TypedQuery<Member> query = em
-                    .createQuery("SELECT m from Member m INNER join m.orders o " +
-                            "where o.createDate = :createDate", Member.class);
+                    .createQuery("SELECT m FROM Member m LEFT OUTER JOIN m.orders", Member.class);
 
             // 페이징 API
             query.setFirstResult(10); // 시작 11부터
             query.setMaxResults(20); // 20개의 데이터 ex. 11~30
             List<Member> resultList = query
-                    .setParameter("createDate", new Date())
                     .getResultList();
 
             tx.commit();
