2d28248c70ecc8d865dd2fe57b78e4c91c211d52
An-Junyeop <thwo0075@gmail.com>
2020-07-18 22:25:04 +0900

    QueryDSL Paging 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.mysema.query.SearchResults;
 import com.mysema.query.jpa.impl.JPAQuery;
 import model.Member;
 import static model.QMember.member;
@@ -6,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Date;
 import java.util.List;
 
 
@@ -21,10 +23,25 @@ public class Main {
 
             JPAQuery query = new JPAQuery(em);
 
-            List<Member> members = query.from(member)
-                    .where(member.name.eq("zzzz"))
-                    .orderBy(member.name.desc())
-                    .list(member);
+            int page = 3;
+            int limit = 5;
+
+            SearchResults<Member> result = query.from(member)
+                    .offset((page - 1) * limit).limit(limit)
+                    .listResults(member);
+
+            long totalCount = result.getTotal();
+            long resultLimit = result.getLimit();
+            long offset = result.getOffset();
+            List<Member> list = result.getResults();
+
+            System.out.println("total count: " + totalCount);
+            System.out.println("offset: " + offset);
+            System.out.println("limit: " + resultLimit);
+
+            for (Member m : list) {
+                System.out.println(m.getId());
+            }
 
             tx.commit();
         } catch (Exception e) {
