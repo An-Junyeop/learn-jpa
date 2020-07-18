5256d484dc3db84875931655d1df1d444d364ed1
An-Junyeop <thwo0075@gmail.com>
2020-07-18 22:30:13 +0900

    QueryDSL Group By 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
-import com.mysema.query.SearchResults;
 import com.mysema.query.jpa.impl.JPAQuery;
-import model.Member;
-import static model.QMember.member;
+import static model.QOrder.order;
+import static model.QOrderItem.orderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.Date;
-import java.util.List;
 
 
 public class Main {
@@ -20,28 +17,12 @@ public class Main {
         try {
             tx.begin();
 
-
             JPAQuery query = new JPAQuery(em);
 
-            int page = 3;
-            int limit = 5;
-
-            SearchResults<Member> result = query.from(member)
-                    .offset((page - 1) * limit).limit(limit)
-                    .listResults(member);
-
-            long totalCount = result.getTotal();
-            long resultLimit = result.getLimit();
-            long offset = result.getOffset();
-            List<Member> list = result.getResults();
-
-            System.out.println("total count: " + totalCount);
-            System.out.println("offset: " + offset);
-            System.out.println("limit: " + resultLimit);
-
-            for (Member m : list) {
-                System.out.println(m.getId());
-            }
+            query.from(orderItem)
+                    .groupBy(orderItem.order.id)
+                    .having(orderItem.count.sum().gt(3))
+                    .list(orderItem);
 
             tx.commit();
         } catch (Exception e) {
