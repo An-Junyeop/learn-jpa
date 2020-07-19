f121db60b0f77d506c2895493983a707159e87c1
An-Junyeop <thwo0075@gmail.com>
2020-07-19 18:41:15 +0900

    Native Query NamedNativeQuery 예제 - XML 사용

--- src/main/java/model/Member.java
+++ src/main/java/model/Member.java
@@ -5,40 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@SqlResultSetMapping(name = "memberWithOrderCount",
-        entities = {
-            @EntityResult(entityClass = model.Member.class, fields = {
-                    @FieldResult(name = "id", column = "m_id"),
-                    @FieldResult(name = "name", column = "m_name"),
-                    @FieldResult(name = "createDate", column = "m_create_date"),
-                    @FieldResult(name = "lastModifiedDate", column = "m_last_date"),
-                    @FieldResult(name = "address.city", column = "m_city"),
-                    @FieldResult(name = "address.street", column = "m_street"),
-                    @FieldResult(name = "address.zipcode", column = "m_zipcode")
-            })
-        },
-        columns = {@ColumnResult(name = "ORDER_COUNT")}
-)
-@NamedNativeQuery(
-        name = "Member.memberWithOrderCount",
-        query =
-                "SELECT M.MEMBER_ID AS M_ID, " +
-                "M.NAME AS M_NAME, " +
-                "M.CREATE_DATE AS M_CREATE_DATE, " +
-                "M.LAST_MODIFIED_DATE AS M_LAST_DATE, " +
-                "M.CITY AS M_CITY, " +
-                "M.STREET AS M_STREET, " +
-                "M.ZIPCODE AS M_ZIPCODE, " +
-                "I.ORDER_COUNT " +
-                "FROM MEMBER M " +
-                "LEFT JOIN " +
-                "   (SELECT MEMBER_ID, COUNT(*) AS ORDER_COUNT " +
-                "   FROM ORDERS " +
-                "   GROUP BY MEMBER_ID " +
-                "   ) I " +
-                "ON M.MEMBER_ID = I.MEMBER_ID",
-        resultSetMapping = "memberWithOrderCount"
-)
 public class Member extends BaseEntity {
 
     @Id
