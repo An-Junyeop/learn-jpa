d3dd57d80b2c9b6c955efe86f1fe326ba55c79fc
An-Junyeop <thwo0075@gmail.com>
2020-07-19 12:44:15 +0900

    QueryDSL 프로젝션과 결과반환 예제
    - 프로젝션 대상이 하나인 경우 예제
    - 여러 컬럼 반환을 튜플로 받는 경우

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,14 @@
+import com.mysema.query.Tuple;
 import com.mysema.query.jpa.JPASubQuery;
 import com.mysema.query.jpa.impl.JPAQuery;
 import com.sun.org.apache.xpath.internal.operations.Or;
-import model.Order;
-import model.QMember;
-import model.QOrder;
-import model.QOrderItem;
+import model.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 
 public class Main {
@@ -23,24 +22,14 @@ public class Main {
 
             JPAQuery query = new JPAQuery(em);
 
-            QOrder order1 = QOrder.order;
-            QOrder subOrder = new QOrder("orderSub");
+            QOrder order = QOrder.order;
+            List<OrderStatus> result = query.from(order).list(order.status);
 
-            query.from(order1)
-                    .where(order1.orderDate.eq(
-                            new JPASubQuery().from(subOrder).unique(subOrder.orderDate.max())
-                    ))
-                    .list(order1);
+            QMember member = QMember.member;
+            List<Tuple> tuples = query.from(member).list(member.id, member.address);
 
-
-            QOrder order2 = new QOrder("order2");
-            QMember subMember = new QMember("memberSub");
-
-            query.from(order2)
-                    .where(order2.member.in(
-                            new JPASubQuery().from(subMember).where(subMember.name.startsWith("kim")).list(subMember)
-                    ))
-                    .list(order2);
+            tuples.get(0).get(member.id);
+            tuples.get(0).get(member.address.city);
 
             tx.commit();
         } catch (Exception e) {
